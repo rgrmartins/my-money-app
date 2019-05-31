d2122db5b127cab851c381c8c45f855892d80682
roger <rogermartinsdesign@gmail.com>
2019-05-31 18:12:06 -0400

    Implementando components e Redux

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright rgrMartins &copy; 2019
+            <a href="http://github.com/rgrmartins" target='_blank'> rgrMartins</a>
+        </strong>
+    </footer>
+)
